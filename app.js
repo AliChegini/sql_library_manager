86914278763c04e710768ce93ebf098b0aca974a
AliChegini <ali.c.fullstack@gmail.com>
2023-03-31 11:32:37 +0200

    final touches

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { sequelize } = require('./models');
 const routes = require('./routes');
@@ -16,7 +15,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
